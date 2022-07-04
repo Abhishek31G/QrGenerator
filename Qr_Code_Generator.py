cefbd89154195daa9406f1b7052d9531f6beab99
Abhishek31G <abhishekgandhewar@gmail.com>
2022-07-04 16:20:00 +0530

    Initial commit

---
+++ Qr_Code_Generator.py
@@ -0,0 +1,7 @@
+import pyqrcode
+
+text = input("Enter the text you want to convert it in a Qr Code? ")
+qr_code = pyqrcode.create(text)
+qr_code.svg("qr_code.svg", module_color='#000', background='#FFF', scale=8)
+
+
